611c38324539968789546464c153ae5a1f9abba4
aqbtech <tvnaquan@gmail.com>
2025-12-01 23:28:22 +0700

    setup: mobile app set up with expo go

---
+++ mobile-app/ballmate/App.tsx
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Chào mừng đến với BallMate</Text>
+      <Text style={styles.subtitle}>
+        Nền tảng đặt sân bóng đá thông minh{'\n'}và hiện đại nhất Việt Nam
+      </Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#297558',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    textAlign: 'center',
+    marginBottom: 16,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#FFFFFF',
+    textAlign: 'center',
+    lineHeight: 24,
+  },
+});
